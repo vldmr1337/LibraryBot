e4fe66a2cd97ab8bfb2f13dd3556a6b0d622a477
vldmr1337 <netok812@gmail.com>
2024-12-17 16:46:43 -0300

    Arrumando de fato os eventos

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,26 +1,54 @@
-const { Events, MessageFlags } = require('discord.js');
+const { Events, Collection, MessageFlags } = require('discord.js');
 
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
 
-		const command = interaction.client.commands.get(interaction.commandName);
+    const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
+    if (!command) {
+      console.error(`Esse comando ${interaction.commandName} não existe/não está registrado.`);
+      return;
+    }
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(error);
-			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-			}
-		}
-	},
+    const { cooldowns } = interaction.client;
+
+    if (!cooldowns.has(command.data.name)) {
+      cooldowns.set(command.data.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.data.name);
+    const defaultCooldownDuration = 3; // segundos
+    const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+    if (timestamps.has(interaction.user.id)) {
+      const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+      if (now < expirationTime) {
+        const expiredTimestamp = Math.round(expirationTime / 1000);
+        return interaction.reply({
+          content: `Estás em cooldown para o comando \`${command.data.name}\`. Tente novamente <t:${expiredTimestamp}:R>.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    timestamps.set(interaction.user.id, now);
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      const replyContent = { content: 'Ocorreu um erro ao executar esse comando.', flags: MessageFlags.Ephemeral };
+
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp(replyContent);
+      } else {
+        await interaction.reply(replyContent);
+      }
+    }
+  },
 };
