78ce45337c224900a96654cc3d311c5c48b59cc6
vldmr1337 <netok812@gmail.com>
2024-12-17 16:37:58 -0300

    Adicionando cooldown aos comandos

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Retorna as informações do usuário que utilizar o comando'),
